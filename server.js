a5d3b56fdc67756823c0ad41012b37943ea51874
Gad-Ongoro <gadongoro1@gmail.com>
2024-05-19 23:33:21 +0300

    feat: Lipa na Mpesa

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // Import Routes
 const userRoutes = require('./routes/users');
 const productsRoute = require('./routes/products');
+const mpesaRoute = require('./routes/mpesa')
 
 
 // Register Routes
@@ -20,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/products', productsRoute);
-
+app.use('/lipa_na_mpesa', mpesaRoute);
 
 // Start server
 app.listen(PORT, () => {
